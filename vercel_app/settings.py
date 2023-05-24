0da56c639ccdd282f40e6c4a08606d989553a56a
Halil İbrahim <haykut19@posta.pau.edu.tr>
2023-05-24 13:55:33 +0300

    changes

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'example',
     'django.forms',
-
+    
 ]
 
 MIDDLEWARE = [
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'vercel_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates',],
+        'DIRS': [BASE_DIR /'templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
