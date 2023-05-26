ecd068732f136be176f60f00a9417f1549bb5625
Furkan AKBAŞ <frknakbas65@hotmail.com>
2023-05-26 23:34:34 +0300

    d

--- example/urls.py
+++ example/urls.py
@@ -1,13 +1,10 @@
 # example/urls.py
 from django.urls import path
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from example.views import index,login,register,logout_request
+
+from example.views import index
 
 
 urlpatterns = [
-    path('', index,name='index'),
-    path('login', login,name='login'),
-    path('register', register,name='register'),
-    path("logout",logout_request, name="logout"),
+    path('', index),
+    
 ]
-urlpatterns += staticfiles_urlpatterns()
