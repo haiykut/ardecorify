dba35bdc12338988cd4180385a3ef9cc55eef212
Furkan AKBAŞ <frknakbas65@hotmail.com>
2023-05-26 23:35:36 +0300

    changes

--- example/urls.py
+++ example/urls.py
@@ -1,10 +1,13 @@
 # example/urls.py
 from django.urls import path
-
-from example.views import index
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from example.views import index,login,register,logout_request
 
 
 urlpatterns = [
-    path('', index),
-    
+    path('', index,name='index'),
+    path('login', login,name='login'),
+    path('register', register,name='register'),
+    path("logout",logout_request, name="logout"),
 ]
+urlpatterns += staticfiles_urlpatterns()
--- example/views.py
+++ example/views.py
@@ -3,30 +3,62 @@ from django.contrib.auth.models import User
 from datetime import datetime
 import json
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 from django.contrib.auth import authenticate,login,logout
 from django.shortcuts import render,redirect
+from django.contrib.auth.models import User, auth
 def index(request):
-    now = datetime.now()
+    time = datetime.now()
     f = open('user.json')
 
-    data = json.load(f)
-    username = data['user']["username"]
-    password = data['user']["password"]
-    user = authenticate(request,username = username,password = password)
-    if user is not None:
-        control=True
-    else:
-        control=False
+
+
+
         
-    html = f'''
-    <html>
-        <body>
-            <h1>Hello from Vercel!</h1>
-            <p>The current time is { now }.</p>,
-            <p>return: { control }.</p>
-        </body>
-    </html>
-    '''   
-        
-    return HttpResponse(html)
+    return render(request, 'index.html',{'time':time})
 HttpResponse("")
+def register(request):
+
+    if request.method == 'POST':
+
+       
+            username = request.POST['username']
+            password= request.POST['password']
+            
+            try:
+                user = User.objects.create_user(username = username , password = password)
+            except:
+                return render(request,'register.html')
+            user.save()
+            
+            return redirect('login')
+
+    else:
+        return render(request,'register.html')
+
+def login(request):
+    
+#   if request.method =="POST":
+#        username = request.POST["username"]  
+#        password = request.POST["password"]
+        if request.method == 'POST':
+            
+            data=request.body.json.loads()
+            username = data['user']["username"]
+            password = data['user']["password"]
+            user = auth.authenticate(username = username, password =password  )
+
+            if user is not None:
+                auth.login(request , user)
+                control=True
+                return redirect('index',{"control":control})    
+            else:
+                control=False
+                return redirect('login',{"control":control})
+                
+        else:
+            return render(request,'login.html')
+
+def logout_request(request):
+    auth.logout(request)
+    return redirect('index')
